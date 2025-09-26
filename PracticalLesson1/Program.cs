3e0c5ed15af96c32b1712283405b7db1f0d6811f
Sherstneva <emili.dan528@gmail.com>
2025-09-26 16:21:12 +0300

    Refactor: Update welcome message

--- PracticalLesson1/Program.cs
+++ PracticalLesson1/Program.cs
@@ -8,7 +8,7 @@ namespace PracticalLesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the To-Do List Application!");
+            Console.WriteLine("Добро to the To-Do List Application!");//////////
 
             var todoManager = new TodoListManager();
 
